5ac3097b36fe082a4f9d4b87a9e853bebf0ae2b2
ZenTommi <tommi.tane@gmail.com>
2025-06-10 19:56:41 +0300

    utilities

---
+++ wzip/my-zip.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]) {
+    if (argc < 2) {
+        printf("wzip: file1 [file2 ...]\n");
+        exit(1);
+    }
+
+    for (int i = 1; i < argc; i++) {
+        FILE *fp = fopen(argv[i], "r");
+        if (fp == NULL) {
+            printf("wzip: cannot open file\n");
+            exit(1);
+        }
+
+        int count = 0;
+        char current = 0, prev = 0;
+        while (fread(&current, sizeof(char), 1, fp) == 1) {
+            if (count == 0 || current == prev) {
+                count++;
+            } else {
+                fwrite(&count, sizeof(int), 1, stdout);
+                fwrite(&prev, sizeof(char), 1, stdout);
+                count = 1;
+            }
+            prev = current;
+        }
+        if (count > 0) { // Write final run
+            fwrite(&count, sizeof(int), 1, stdout);
+            fwrite(&prev, sizeof(char), 1, stdout);
+        }
+        fclose(fp);
+    }
+    return 0;
+}
