73caf053d8f8b98a958ee4f4a04e9f516192f705
Kuutteri <128422877+Kuutteri@users.noreply.github.com>
2025-06-11 15:36:18 +0300

    Add files via upload

---
+++ my-grep.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(int argc, char *argv[]) {
+    if (argc < 2) {
+        printf("my-grep: searchterm [file ...]\n");
+        exit(1);
+    }
+
+    char *pattern = argv[1];
+    char *buffer = NULL;
+    size_t buf_size = 0;
+
+    if (argc != 2) {
+        int arg_index = 2;
+        while (arg_index < argc) {
+            FILE *file = fopen(argv[arg_index], "r");
+            if (file == NULL) {
+                printf("my-grep: cannot open file\n");
+                exit(1);
+            }
+            while (getline(&buffer, &buf_size, file) != -1) {
+                if (strstr(buffer, pattern) != NULL) {
+                    printf("%s", buffer);
+                }
+            }
+            fclose(file);
+            arg_index++;
+        }
+    } else {
+        while (getline(&buffer, &buf_size, stdin) != -1) {
+            if (strstr(buffer, pattern) != NULL) {
+                printf("%s", buffer);
+            }
+        }
+    }
+    free(buffer);
+    return 0;
+}
